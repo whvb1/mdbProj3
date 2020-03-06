8aa5dd62610f62ae75005fc854b2fe8f7c4eb733
William Vavrik <whvb1@berkeley.edu>
2020-03-05 21:47:57 -0800

    getting location of phone

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        System.out.println(location);
+                        if (location != null) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "text", Toast.LENGTH_LONG);
+                            toast.show();
+
+                        }
+                    }
+                });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/Models/weather.java
@@ -0,0 +1,22 @@
+package com.example.weatherapp.Models;
+
+public class weather {
+    /*need to add four 0s to the time
+    * to get the current time since EPOCH
+    * hmmm interesting ex. 1583910000 + 0000 = 15839100000000 (March 11 2020 8pm)
+     */
+    private String name;
+    private int latitude;
+    private int longitude;
+    private String summary;
+    private int temperature;
+    private String icon;
+    private int moonPhase;
+    private int visibility;
+    private String timezone;
+    private int nearestStormDistance;
+
+
+
+
+}
